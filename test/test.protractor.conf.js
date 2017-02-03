1df775191cf22775ece85c4bc81cf8cbb7d4f36a
Heltonss <helton@heltonsouza.com.br>
2017-02-03 13:36:30 -0200

    protractor executando testes no terminal - nomeei essa branch errado =/

--- test/test.protractor.conf.js
+++ test/test.protractor.conf.js
@@ -1,8 +1,18 @@
 exports.config = {
-    seleniumServerJar: '../node_modules/webdriver-manager/selenium/selenium-server-standalone-2.53.1.jar',
-    // seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['todo-spec.js'],
-    chromeOnly: true,
-    showColors: true
+    framework: 'jasmine',
+    // seleniumServerJar: '../node_modules/webdriver-manager/selenium/selenium-server-standalone-2.53.1.jar',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['e2e/*.js'],
+    // chromeOnly: true,
+    showColors: true,
+    capabilities: {
+        'browserName': 'phantomjs',
+        'phantomjs.binary.path': require('phantomjs-prebuilt').path,
+    },
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 30000,
+        showColors: true
+    },
+    allScriptsTimeout: 30000
 
 };
