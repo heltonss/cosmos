952ba3e7e3f3a61099d2872e3b98c364f5216114
Heltonss <helton@heltonsouza.com.br>
2017-02-09 22:03:10 -0200

    3 test Karma CI com node modules

--- test.karma.continuous.integration.js
+++ test.karma.continuous.integration.js
@@ -15,8 +15,8 @@ module.exports = function(config) {
 
         // list of files / patterns to load in the browser
         files: [
-            '/node_modules/angular/angular.js',
-            '/node_modules/angular-mocks/angular-mocks.js',
+            '../node_modules/angular/angular.js',
+            '../node_modules/angular-mocks/angular-mocks.js',
             // 'app/bower_components/angular-route/angular-route.js',
             // 'app/bower_components/jquery/dist/jquery.js',
             // // '../app/bower_components/materialize/bin/materialize.js',
