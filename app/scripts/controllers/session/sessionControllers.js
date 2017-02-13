af4440a16c265bf74f690b1379ece2d40d3a24b6
Heltonss <helton@heltonsouza.com.br>
2017-02-13 12:13:09 -0200

    Menu finalizado

--- app/scripts/controllers/session/sessionControllers.js
+++ app/scripts/controllers/session/sessionControllers.js
@@ -8,12 +8,12 @@ sessionControllers.controller('sessionCtrl', ['$location',
         vm.sessionValidate = sessionValidate;
 
         function sessionValidate(user) {
-            vm.login = user.login;
-            vm.pass = user.password;
+            // vm.login = user.login;
+            // vm.pass = user.password;
 
-            if (vm.login == "helton" && vm.pass == "123456") {
-                $location.path('/secretaria');
-            }
+            // if (vm.login == "helton" && vm.pass == "123456") {
+            $location.path('/secretaria');
+            // }
         }
 
     }
