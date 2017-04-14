93b060a4a04dccf3d09a3def5ec280a629a2a50d
Heltonss <helton@heltonsouza.com.br>
2017-04-14 15:06:56 -0300

    form crud colaborador finalizado

--- app/scripts/services/colaborador/service-colaborador.js
+++ app/scripts/services/colaborador/service-colaborador.js
@@ -4,7 +4,7 @@ var colaboradorService = angular.module('colaboradorService', ['ngResource']);
 
 colaboradorService.factory('crudColaborador', ['$resource',
     function ($resource) {
-        const urlEndPoint = 'http://localhost:5000/'
+        var urlEndPoint = 'http://localhost:5000/'
         return $resource("", {}, {
             get: {
                 method: 'GET', url: urlEndPoint + 'administrador/:id', cache: false, isArray: false
