3a9899a551c1b0b22391da87dc1e7b0b4d5dc38e
Heltonss <helton@heltonsouza.com.br>
2017-04-14 11:21:41 -0300

    controller crud finalizado

--- app/scripts/controllers/ColaboratorForm/colaboradorFormController.js
+++ app/scripts/controllers/ColaboratorForm/colaboradorFormController.js
@@ -3,8 +3,27 @@
 var colaboradorFormControllers = angular.module('colaboradorFormControllers', []);
 
 
-colaboradorFormControllers.controller('colaboradorFormCtrl', [
-    function colaboradorFormCtrl() {
+colaboradorFormControllers.controller('colaboradorFormCtrl', ['$scope', 'crudColaborador', '$routeParams',
+    function colaboradorFormCtrl($scope, crudColaborador, $routeParams) {
+        var vm = this;
+
+        vm.create = create;
+
+
+
+        function create(colaborador) {
+            vm.colaborador = colaborador;
+
+            var createColaborador = JSON.stringify(vm.colaborador, null, " ");
+            crudColaborador.save(createColaborador,
+                function success(res) {
+                    console.log("colaborador salvo no banco")
+                }),
+                function error(resError) {
+                    console.log("erro" + JSON.stringify(resError));
+                }
+
+        }
 
     }
 ])
