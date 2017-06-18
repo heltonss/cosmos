77d0843c2fc957c7be6400f9cbc5eb5286e42545
Heltonss <helton@heltonsouza.com.br>
2017-06-18 14:23:10 -0300

    teste

--- app/scripts/controllers/session/sessionControllers.js
+++ app/scripts/controllers/session/sessionControllers.js
@@ -4,8 +4,16 @@ var sessionControllers = angular.module('sessionControllers', []);
 
 sessionControllers.controller('sessionCtrl', ['$location',
     function sessionCtrl($location) {
-  
-        
+        var vm = this;
 
+        vm.sessionValidate = sessionValidate;
+        function sessionValidate(user) {
+            if (user.login === "user" && user.password == '@xpto123') {
+                sessionStorage.setItem('username', user.login);
+                sessionStorage.setItem('password', user.password);
+                $location.path('/dashboard');
+                console.log('login errado');
+            }
+        }
     }
 ]);
